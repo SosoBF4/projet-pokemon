e54ce651ccfad93cfdfb3605144f312281e10034
SosoBF4 <sofianefortnite66@gmail.com>
2026-01-17 17:23:28 +0100

    V4

---
+++ sql/Mettre_element.sql
@@ -0,0 +1,59 @@
+/*INSERT INTO pokemon (id_pokemon, nom, pv_max, attaque, defense, vitesse, image, id_type) VALUES
+-- 🌱 PLANTE (id_type = 3)
+(1, 'Bulbizarre', 50, 48, 50, 45, './image/1.png', 3),
+(2, 'Herbizarre', 65, 60, 65, 60, './image/2.png', 3),
+(3, 'Florizarre', 90, 85, 90, 80, './image/3.png', 3),
+(4, 'Chenipan', 45, 30, 35, 45, './image/10.png', 3),
+(5, 'Chrysacier', 55, 25, 60, 30, './image/11.png', 3),
+(6, 'Papilusion', 70, 60, 55, 70, './image/12.png', 3),
+(7, 'Aspicot', 45, 35, 30, 50, './image/13.png', 3),
+(8, 'Coconfort', 55, 30, 60, 35, './image/14.png', 3),
+(9, 'Dardargnan', 75, 90, 65, 75, './image/15.png', 3),
+(10, 'Mystherbe', 45, 45, 50, 30, './image/43.png', 3),
+(11, 'Ortide', 65, 60, 65, 40, './image/44.png', 3),
+(12, 'Rafflesia', 90, 85, 90, 50, './image/45.png', 3),
+(13, 'Boustiflor', 65, 60, 65, 50, './image/69.png', 3),
+(14, 'Empiflor', 90, 90, 85, 60, './image/70.png', 3),
+(15, 'Chétiflor', 50, 48, 50, 45, './image/71.png', 3),
+
+-- 🔥 FEU (id_type = 2)
+(16, 'Salamèche', 45, 52, 43, 65, './image/4.png', 2),
+(17, 'Reptincel', 65, 64, 58, 80, './image/5.png', 2),
+(18, 'Dracaufeu', 90, 95, 85, 100, './image/6.png', 2),
+(19, 'Goupix', 45, 45, 40, 65, './image/37.png', 2),
+(20, 'Feunard', 80, 85, 80, 100, './image/38.png', 2),
+(21, 'Caninos', 60, 70, 55, 80, './image/55.png', 2),
+(22, 'Arcanin', 95, 100, 85, 95, './image/56.png', 2),
+
+-- 💧 EAU (id_type = 1)
+(23, 'Carapuce', 50, 48, 65, 43, './image/7.png', 1),
+(24, 'Carabaffe', 70, 63, 80, 58, './image/8.png', 1),
+(25, 'Tortank', 100, 85, 105, 78, './image/9.png', 1),
+(26, 'Psykokwak', 55, 52, 50, 55, './image/54.png', 1),
+(27, 'Akwakwak', 90, 82, 80, 85, './image/55.png', 1),
+(28, 'Ptitard', 45, 45, 45, 90, './image/60.png', 1),
+(29, 'Têtarte', 70, 65, 70, 90, './image/61.png', 1),
+(30, 'Tentacool', 50, 45, 60, 70, './image/72.png', 1),
+(31, 'Tentacruel', 90, 75, 85, 80, './image/73.png', 1),
+(32, 'Krabby', 50, 85, 60, 50, './image/98.png', 1),
+(33, 'Krabboss', 85, 105, 100, 75, './image/99.png', 1),
+
+-- ⚡ ELECTRIK (id_type = 4)
+(34, 'Pikachu', 45, 55, 40, 90, './image/25.png', 4),
+(35, 'Raichu', 75, 90, 65, 110, './image/26.png', 4),
+(36, 'Magnéti', 40, 35, 70, 45, './image/81.png', 4),
+(37, 'Voltorbe', 50, 35, 55, 100, './image/100.png', 4),
+(38, 'Magnéton', 65, 60, 95, 70, './image/82.png', 4),
+(39, 'Évoli', 55, 55, 50, 55, './image/133.png', 4),
+(40, 'Voltali', 75, 90, 60, 130, './image/135.png', 4),
+(41, 'Élektek', 80, 95, 65, 105, './image/125.png', 4);
+
+
+
+INSERT INTO `type` (`id_type`, `nom_type`) VALUES
+(1, 'Eau'),
+(2, 'Feu'),
+(3, 'Plante'),
+(4, 'Electrik');
+
+*/
